384543bb4646dfc570c337c3175a6c0eb33fd30e
Davide Laghi <davide.laghi01@gmail.com>
2024-05-02 17:57:11 +0200

    added all oktavian tallies

--- app_streamlit.py
+++ app_streamlit.py
@@ -183,7 +183,7 @@ def main():
     # -- Application --
     col01, col02 = st.columns([0.9, 0.1])
     with col01:
-        st.title("JADE results interactive plotter [ALPHA]")
+        st.title("JADE results interactive plotter [BETA]")
     with col02:
         st.image(r"jadewa/assets/Jade.png", width=75)
 
